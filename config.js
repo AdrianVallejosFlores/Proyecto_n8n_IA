ead8900cc7057a4c69d48f8962ffd45075aaebf8
AdrianVallejosFlores <adrianvallejosflores@gmail.com>
2025-11-25 23:38:35 -0400

    feat: nueva url del otro flujo alternativo

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.ENV = {
-    WEBHOOK_URL: "https://adrian-vallejos-dev.app.n8n.cloud/webhook/e7058a2d-2958-49a5-853b-57b2ba994a08"
+    WEBHOOK_URL_1: "https://adrian-vallejos-dev.app.n8n.cloud/webhook/e7058a2d-2958-49a5-853b-57b2ba994a08",
+    WEBHOOK_URL_2: "https://adrian-vallejos-dev.app.n8n.cloud/webhook/b7e66f72-ec2a-4d99-9629-26e6b5bbfc4b"
 
 }
